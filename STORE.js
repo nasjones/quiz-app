ffc4a959ff273217a1981a0537a170fe15a20107
nasjones <nasjonez1@gmail.com>
2019-10-19 09:49:39 -0400

    Full quiz app push

---
+++ STORE.js
@@ -0,0 +1,103 @@
+const questionsArray = [
+    //#1
+    {
+        quest: "Who took care of the Elric Brothers after their mother died?",
+        choices: [
+            "Pinako Rockbell",
+            "Trisha Elric",
+            "Rosé Thomas",
+            "Maes Hughes"
+        ],
+        correctAnswer: "Pinako Rockbell",
+    },
+    //#2
+    {
+        quest: "Who is the FullMetal Alchemist?",
+        choices: [
+            "Edward Elric",
+            "Winry Rockbell",
+            "Alphonse Elric",
+            "Roy Mustang"
+        ],
+        correctAnswer: "Edward Elric"
+    },
+    //#3
+    {
+        quest: "What date is written in Edwards pocket watch?",
+        choices: [
+            "3.Oct.10",
+            "9.Sep.19",
+            "1.Jan.11",
+            "4.jun.16"
+        ],
+        correctAnswer: "3.Oct.10"
+    },
+    //#4
+    {
+        quest: "What is general Armstrongs nickname?",
+        choices: [
+            "The Strong Arm Alchemist",
+            "The Gentle Alchemist",
+            "The Crimson Alchemist",
+            "The Flame Alchemist"
+        ],
+        correctAnswer: "The Strong Arm Alchemist"
+    },
+    //#5
+    {
+        quest: "Who is the Elric brothers father?",
+        choices: [
+            "Roy Mustang",
+            "Tim Marcoh",
+            "Izumi Curtis",
+            "Van Hohenheim"
+        ],
+        correctAnswer: "Van Hohenheim"
+    },
+    //#6
+    {
+        quest: "Who was NOT a part of roy mustangs team?",
+        choices: [
+            "Jean Havoc",
+            "Riza Hawkeye",
+            "Isaac McDougal",
+            "Vato Falman"
+        ],
+        correctAnswer: "Isaac McDougal"
+    },
+    //#7
+    {
+        quest: "What were the Elric Brothers doing that cost them their bodies?",
+        choices: [
+            "Looking for their father",
+            "Trying to make diamond out of coal",
+            "Trying to bring their mother back to life",
+            "Trying to create the philosopher stone"
+        ],
+        correctAnswer: "Trying to bring their mother back to life"
+    },
+    //#8
+    {
+        quest: "What is Edward's biggest insecurity?",
+        choices: [
+            "His weight",
+            "His teeth",
+            "His height",
+            "His intellect"
+        ],
+        correctAnswer: "His height"
+    },
+    //#9
+    {
+        quest: "What elemental alchemy does Roy Mustang have control over?",
+        choices: [
+            "Water",
+            "Fire",
+            "Earth",
+            "Air"
+        ],
+        correctAnswer: "Fire"
+    }
+]
+
+const length = questionsArray.length;
---
+++ script.js
@@ -0,0 +1,126 @@
+let questNum;
+let grade;
+let progress;
+let myArray;
+let currentQuestion;
+let hasItRun = false;
+
+
+function initialState() {
+    myArray = questionsArray.slice(0);
+    grade = 0;
+    progress = 1;
+    questNum = questionsArray.length;
+}
+
+function getQuestion() {
+    let randomNum = Math.floor(Math.random() * questNum);
+    currentQuestion = myArray.splice(randomNum, 1);
+    questNum--;
+    quizBoxHtml(currentQuestion[0]);
+    progressUpdate();
+}
+
+function quizBoxHtml(pass) {
+    $('#quiz-box').empty();
+    $('#quiz-box').append(`<h4 id="quiz-question">${pass.quest}</h4><div id="question-container">`);
+    let tempChoices = pass.choices.slice(0);
+    for (var i = pass.choices.length - 1; i >= 0; i--) {
+        let r = Math.floor(Math.random() * (i + 1));
+        currentChoice = tempChoices.splice(r, 1);
+        $('#question-container').append(`<label><input id="choice" type="radio" name="choice" value="${currentChoice}">${currentChoice}</label><br>`);
+    }
+    $('#quiz-box').append(`</div><button type="button" id="submit">Submit</button>`);
+}
+
+function questionCheck() {
+    $('#quiz-box').on('click', '#submit', e => {
+        e.preventDefault();
+        let chosen = $(e.currentTarget).parent().find('input[name=choice]:checked');
+        if (chosen.val()) {
+            if (chosen.val() === currentQuestion[0].correctAnswer) {
+                $('#quiz-box').html('<p>That is correct</p> <button type="button" id="next">Next>></button>');
+                reactionAdd("positive");
+                grade++;
+            } else {
+                $('#quiz-box').html(`<p>Sorry that is incorrect. The correct answer was '${currentQuestion[0].correctAnswer}'</p> <button type="button" id="next">Next>></button>`);
+                reactionAdd("negative");
+            }
+        } else {
+            alert("Please make a choice.");
+        }
+        progressUpdate();
+        nextQuestion();
+    });
+}
+
+function progressUpdate() {
+    $('.progress').html(`<h2>Question ${progress} of ${length}</h2><h3>Your current score is ${grade}</h3>`);
+}
+
+function nextQuestion() {
+    if (questNum != 0) {
+        $('#next').on('click', e => {
+            e.preventDefault();
+            progress++;
+            $('#quiz-box').empty();
+            $('.reaction').empty();
+            getQuestion();
+        });
+    } else {
+        $('#next').on('click', e => {
+            e.preventDefault();
+            $('.reaction').empty();
+            $('#quiz-box').empty();
+            finalScore();
+        });
+    }
+}
+
+function reactionAdd(reactType) {
+    if (reactType === "positive") {
+        $('#quiz-box').append('<img src="https://data.whicdn.com/images/33408514/original.gif">');
+    }
+    else {
+        $('#quiz-box').append('<img src="https://media2.giphy.com/media/1hPYD3gsL8wZa/source.gif">');
+    }
+
+}
+
+function finalScore() {
+    $('.progress').empty();
+    $('#quiz-box').append(
+        `<p class = "results">You've finished the quiz your final results are ${grade}/${length}<br>Click the button below if you'd like to take it again!</p>  
+      <button type="button" id="reset">Reset</button>`
+    );
+    $('#reset').on('click', e => {
+        $('#intro-pic').show();
+        $('#prompt').show();
+        $('#quiz-box').hide();
+    });
+}
+
+function begin() {
+    $('#begin').on('click', e => {
+        if (!hasItRun) {
+            e.preventDefault();
+            initialState();
+            $('#intro-pic').hide();
+            $('#prompt').hide();
+            $('#quiz-box').show();
+            getQuestion();
+            questionCheck();
+            hasItRun = true;
+        }
+        else {
+            e.preventDefault();
+            initialState();
+            getQuestion();
+            $('#intro-pic').hide();
+            $('#prompt').hide();
+            $('#quiz-box').show();
+        }
+
+    });
+}
+$(begin);
